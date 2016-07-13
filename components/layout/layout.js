07a21f0a2c199558d1592fff89a0c1503abf45a2
fran150 <franllopez@yahoo.com.ar>
2016-07-13 01:33:47 -0300

    Delete unused components

---
+++ components/layout/layout.js
@@ -0,0 +1,45 @@
+define(['quark', 'knockout', 'text!./layout.html'], function($$, ko, template) {
+    return $$.component(function(params, $scope) {
+        var self = this;
+
+        $$.parameters({
+            sidebarSize: ko.observable(20),
+            containerSize: ko.observable('md'),
+            hasNavbar: false,
+            hasSidebar: false,
+            minSidebarSize: ko.observable(20)
+        }, params, this);
+
+        this.initComponent = function() {
+            validateSize(20);
+        }
+
+        function validateSize(size) {
+            var minSize = self.minSidebarSize();
+            var maxSize = $(window).width() / 2;
+
+            if ($$.isNumeric(minSize) && minSize > 0 && size < minSize) {
+                self.sidebarSize(minSize);
+            }
+
+            if (size < 0) {
+                self.sidebarSize(0);
+            }
+
+            if (size > maxSize) {
+                self.sidebarSize(maxSize);
+            }
+        }
+
+        var subscriptions = {
+            sidebarSize: self.sidebarSize.subscribe(function(newValue) {
+                validateSize(newValue);
+            })
+        };
+
+        // Al eliminar el componente limpia las subscripciones y listeners
+        this.dispose = function() {
+            subscriptions.sidebarSize.dispose();
+        }
+    }, template)
+})
--- components/spinner/spinner.js
+++
@@ -1,6 +0,0 @@
-define(['knockout', 'quark', 'text!./spinner.html'], function (ko, $$, template) {
-    return $$.component(function(params, $scope) {
-        var self = this;
-
-    }, template);
-});
